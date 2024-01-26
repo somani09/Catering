3dc8e004cebd6afbb6bc294a0fb260c7576424cc
Vaibhav Somani <vsomani2@gmail.com>
2024-01-26 16:30:44 -0700

    Added navbar

---
+++ src/components/header/header.tsx
@@ -0,0 +1,121 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../../public/svg/logo.svg";
+import styles from "./header.module.scss";
+import user from "../../../public/svg/user.svg";
+
+const header: React.FC = () => {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200  dark:bg-gray-800 h-20 md:h-32 relative px-3 lg:px-24">
+        <div className="flex flex-wrap justify-between items-center h-full relative">
+          <Link
+            href="/"
+            className={`flex items-center h-14 w-14 md:h-24 md:w-24 ${styles.logo}`}
+          >
+            <Image src={logo} alt="Your SVG" objectFit="contain" />
+          </Link>
+          <div className="flex items-center lg:order-2">
+            {/* <Link
+              href="#"
+              className={`text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 ${styles.user_options}`}
+            >
+              <Image src={user} alt="Your SVG" objectFit="contain" />
+            </Link> */}
+
+            <Link
+              href="/"
+              className={`flex items-center h-14 w-14 md:h-24 md:w-24 ${styles.logo}`}
+            >
+              <Image src={user} alt="Your SVG" objectFit="contain" />
+            </Link>
+
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row  lg:mt-0">
+              <li className="lg:px-10 lg:border-r-2">
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 navbar-text border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About
+                </Link>
+              </li>
+              <li className="lg:px-10 lg:border-r-2">
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 navbar-text border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Services
+                </Link>
+              </li>
+              <li className="lg:px-10 lg:border-r-2">
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 navbar-text border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Dishes
+                </Link>
+              </li>
+              <li className="lg:px-10 lg:border-r-2">
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 navbar-text border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="lg:px-10">
+                <Link
+                  href="#"
+                  className="block py-2 pr-4 pl-3 navbar-text border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default header;
