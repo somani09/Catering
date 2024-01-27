05e12d4e28d3f1841810343906ad3a5f27f15b07
Vaibhav Somani <vsomani2@gmail.com>
2024-01-26 19:32:55 -0700

    completed dishes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./home.module.scss";
 import background from "../../../public/svg/background_food.svg";
 import ServiceBar from "./serviceBar";
+import Dishes from "./dishes";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       </div>
 
       <ServiceBar />
+      <Dishes />
     </main>
   );
 }
